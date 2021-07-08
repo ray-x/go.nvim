5e37dc67e93bb0c9a41f8e9bacb6e8ceacd511d9
ray-x <rayx.cn@gmail.com>
2021-07-09 02:52:06 +1000

    allow specify path of test

--- lua/go/gotest.lua
+++ lua/go/gotest.lua
@@ -3,7 +3,7 @@ local utils = require("go.utils")
 
 M.test_fun = function(args)
 
-  local cwd = vim.fn.getcwd(0)
+  local fpath = vim.fn.expand('%:p:h')
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   row, col = row + 1, col + 1
   local ns = require("go.ts.go").get_func_method_node_at_pos(row, col)
@@ -12,7 +12,7 @@ M.test_fun = function(args)
   end
 
   utils.log("parnode" .. vim.inspect(ns))
-  local cmd = [[setl makeprg=go\ test\ -v\ -run\ ^]] .. ns.name
+  local cmd = [[setl makeprg=go\ test\ -v\ -run\ ^]] .. ns.name .. [[\ ]] .. fpath
                   .. [[ | lua require"go.asyncmake".make()]]
   utils.log("test cmd", cmd)
   vim.cmd(cmd)
