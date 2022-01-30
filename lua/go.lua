a853613467b685ae5a9d212d2558b92abbfc93f9
ray-x <rayx.cn@gmail.com>
2022-01-30 15:57:56 +1100

    use `-1` for random port

--- lua/go.lua
+++ lua/go.lua
@@ -40,7 +40,7 @@ _GO_NVIM_CFG = {
   dap_debug_keymap = true, -- true: use keymap for debugger defined in go/dap.lua
   -- false: do not use keymap in go/dap.lua.  you must define your own.
   dap_vt = true, -- false, true and 'all frames'
-  dap_port = 38697, -- can be set to a number or 'random' so go.nvim will pickup a random port
+  dap_port = 38697, -- can be set to a number or `-1` so go.nvim will pickup a random port
   build_tags = "", --- you can provide extra build tags for tests or debugger
   textobjects = true,
   test_runner = "go", -- richgo, go test, richgo, dlv, ginkgo
