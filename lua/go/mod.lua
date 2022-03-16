ef32d8914b51a07505e5cf3763c4aee164f9c560
ray-x <rayx.cn@gmail.com>
2022-03-17 00:05:07 +1100

    allow multipls arguments for go mod

--- lua/go/mod.lua
+++ lua/go/mod.lua
@@ -1,18 +1,20 @@
 local runner = require("go.runner")
+local utils = require("go.utils")
 local M = {}
 
 -- args: tidy or vendor
-function M.run(args)
+function M.run(...)
+  local args = { ... }
   local cmd = { "go", "mod" }
   vim.list_extend(cmd, args)
   local opts = {
     after = function()
       vim.schedule(function()
         utils.restart()
-	  end)
+      end)
     end,
   }
-  runner.run(cmd)
+  runner.run(cmd, opts)
 end
 
 return M
