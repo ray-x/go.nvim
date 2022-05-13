7c434918be0716ca1cf5b68be2f872ed5ca344f4
ray-x <rayx.cn@gmail.com>
2022-05-13 19:27:02 +1000

    add gonvim project file parser

--- lua/go/utils.lua
+++ lua/go/utils.lua
@@ -510,4 +510,42 @@ util.deletedir = function(dir)
   util.log("remove dir", dir)
 end
 
+function util.file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+function util.lines_from(file)
+  if not util.file_exists(file) then return {} end
+  local lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+function util.set_env(key, val)
+end
+
+function util.list_directory()
+  local fn = vim.fn
+  local dirs = fn.map(fn.glob(fn.fnameescape('./')..'/{,.}*/', 1, 1), 'fnamemodify(v:val, ":h:t")')
+end
+
+function util.set_nulls()
+  if _GO_NVIM_CFG.null_ls_document_formatting_disable then
+    local query = {}
+    if type( _GO_NVIM_CFG.null_ls_document_formatting_disable) ~= 'boolean'
+    then
+      query = _GO_NVIM_CFG.null_ls_document_formatting_disable
+    end
+    local ok, nulls = pcall(require, "null-ls")
+    if ok then
+      nulls.disable(query)
+    end
+  end
+end
 return util
