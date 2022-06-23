111225f199bde10d7d3fa32e821d5d92af2bd085
ray-x <rayx.cn@gmail.com>
2022-06-23 14:44:42 +1000

    add side_panel for go package

--- lua/go/package.lua
+++ lua/go/package.lua
@@ -2,6 +2,7 @@ local golist = require("go.list").list
 local util = require("go.utils")
 local log = util.log
 local vfn = vim.fn
+local api = vim.api
 local complete = function()
   local ok, l = golist(false, { util.all_pkgs() })
   if not ok then
@@ -39,11 +40,115 @@ end
 
 local pkg_from_path = function(arg)
   log(arg, path)
-  return util.exec_in_path({'go', 'list'})
+  return util.exec_in_path({ "go", "list" })
+end
+
+local show_float = function(result)
+  local textview = util.load_plugin("guihua.lua", "guihua.textview")
+  if not textview then
+    util.log("Failed to load guihua.textview")
+
+    vim.fn.setloclist({}, " ", {
+      title = "go package outline",
+      lines = result,
+    })
+    vim.cmd("lopen")
+    return
+  end
+  local win = textview:new({
+    relative = "cursor",
+    syntax = "lua",
+    rect = { height = math.min(40, #result), pos_x = 0, pos_y = 10 },
+    data = result,
+  })
+  log("draw data", result)
+  vim.api.nvim_buf_set_option(win.buf, "filetype", "go")
+  return win:on_draw(result)
+end
+
+local show_panel = function(result)
+  local fname = vim.fn.tempname() .. "._go" -- avoid lsp activation
+  log("tmp:" .. fname)
+  local uri = vim.uri_from_fname(fname)
+  local bufnr = vim.uri_to_bufnr(uri)
+  vim.fn.writefile(result, fname)
+  vfn.bufload(bufnr)
+  local defs = require("go.ts.utils").list_definitions_toc(bufnr)
+
+  log("defs", defs)
+  local panel = util.load_plugin("guihua.lua", "guihua.panel")
+  if panel then
+    local p = panel:new({
+      header = "go pkg info",
+      render = function(b)
+        log("render for ", bufnr, b)
+        return defs
+      end,
+    })
+    p:open(true)
+  else
+    vim.fn.setloclist({}, " ", {
+      title = "go package outline",
+      lines = defs,
+    })
+    vim.cmd("lopen")
+  end
+
+  vim.api.nvim_buf_delete(bufnr, { unload = true })
+  os.remove(fname)
+end
+-- get package info
+
+local package_outline = function(...)
+  local listcmd = { "go", "list", "./..." }
+  local pkg = util.exec_in_path(listcmd)
+  if vfn.empty(pkg) == 1 then
+    util.log("No package found in current directory.")
+    return nil
+  end
+  local arg = select(1, ...)
+  local setup = { "go", "doc", "-all", "-u", "-cmd", pkg[1] }
+  local result = {}
+  vfn.jobstart(setup, {
+    on_stdout = function(_, data, _)
+      data = util.handle_job_data(data)
+      if not data then
+        return
+      end
+      local types = { "CONSTANTS", "FUNCTIONS", "TYPES", "VARIABLES" }
+      for _, val in ipairs(data) do
+        -- first strip the filename
+        log(val)
+        if vim.tbl_contains(types, val) then
+          val = "//" .. val
+        end
+
+        local sp = string.match(val, "(%s*)")
+        if sp and #sp == 4 then
+          val = "//" .. val
+        end
+        -- todo search in current dir with lsp workspace symbols
+        table.insert(result, val)
+      end
+    end,
+    on_exit = function(_, data, _)
+      if data ~= 0 then
+        vim.notify("no packege info data", vim.lsp.log_levels.WARN)
+        return
+      end
+
+      if arg == "-f" then
+        return show_float(result)
+      end
+
+      show_panel(result)
+    end,
+  })
 end
 
 return {
   complete = complete,
   all_pkgs = all_pkgs,
   pkg_from_path = pkg_from_path,
+  package_outline = package_outline,
 }
--- lua/go/ts/utils.lua
+++ lua/go/ts/utils.lua
@@ -1,4 +1,10 @@
+local api = vim.api
+
+local ts_query = vim.treesitter.query
+local ts_utils = require'nvim-treesitter.ts_utils'
+local log = require('go.utils').log
 local M = {}
+
 -- local ulog = require("go.utils").log
 M.intersects = function(row, col, sRow, sCol, eRow, eCol)
   -- ulog(row, col, sRow, sCol, eRow, eCol)
@@ -15,6 +21,104 @@ M.intersects = function(row, col, sRow, sCol, eRow, eCol)
   end
 
   return true
+end
+
+
+local locals = require'nvim-treesitter.locals'
+-- copy from nvim-treesitter/treesitter-refactor plugin
+--- Get definitions of bufnr (unique and sorted by order of appearance).
+local function get_definitions(bufnr)
+  local local_nodes = locals.get_locals(bufnr)
+
+  -- Make sure the nodes are unique.
+  local nodes_set = {}
+  for _, loc in ipairs(local_nodes) do
+    if loc.definition then
+      locals.recurse_local_nodes(loc.definition, function(_, node, _, match)
+        -- lua doesn't compare tables by value,
+        -- use the value from byte count instead.
+        local _, _, start = node:start()
+        nodes_set[start] = { node = node, type = match or "" }
+      end)
+    end
+  end
+
+  -- Sort by order of appearance.
+  local definition_nodes = vim.tbl_values(nodes_set)
+  table.sort(definition_nodes, function(a, b)
+    local _, _, start_a = a.node:start()
+    local _, _, start_b = b.node:start()
+    return start_a < start_b
+  end)
+
+  return definition_nodes
+end
+
+-- a hack to treesitter-refactor plugin to return list node for outline
+function M.list_definitions_toc(bufnr)
+  bufnr = bufnr or api.nvim_win_get_buf(api.nvim_get_current_win())
+  vim.api.nvim_buf_set_option(bufnr, "filetype", "go")
+  local definitions = get_definitions(bufnr)
+
+  if #definitions < 1 then
+    return
+  end
+
+  local loc_list = {}
+
+  -- Force some types to act like they are parents
+  -- instead of neighbors of the next nodes.
+  local containers = {
+    ["function"] = true,
+    ["type"] = true,
+    ["method"] = true,
+  }
+
+  local parents = {}
+
+  for _, def in ipairs(definitions) do
+    -- Get indentation level by putting all parents in a stack.
+    -- The length of the stack minus one is the current level of indentation.
+    local n = #parents
+    for i = 1, n do
+      local index = n + 1 - i
+      local parent_def = parents[index]
+      if
+        ts_utils.is_parent(parent_def.node, def.node)
+        or (containers[parent_def.type] and ts_utils.is_parent(parent_def.node:parent(), def.node))
+      then
+        break
+      else
+        parents[index] = nil
+      end
+    end
+    parents[#parents + 1] = def
+
+    local lnum, col, _ = def.node:start()
+    local type = def.type
+    -- local kind = string.upper(def.type:sub(1, 1))
+    local text = ts_query.get_node_text(def.node, bufnr) or ""
+
+    local line_text = api.nvim_buf_get_lines(bufnr, lnum, lnum + 1, false)[1] or text
+    log(text)
+    table.insert(loc_list, {
+      bufnr = bufnr,
+      -- lnum = lnum + 1,
+      col = col + 1,
+      indent_level = #parents,
+      hint = line_text,
+      text = text,
+      type = type,
+      -- kind = kind,
+    })
+  end
+  return loc_list
+
+  -- vim.fn.setloclist(winnr, loc_list, "r")
+  -- -- The title needs to end with `TOC`,
+  -- -- so Neovim displays it like a TOC instead of an error list.
+  -- vim.fn.setloclist(winnr, {}, "a", { title = "Definitions TOC" })
+  -- api.nvim_command "lopen"
 end
 
 return M
