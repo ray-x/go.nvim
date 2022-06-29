42ab3fe1072376d5f93224f4746d68671536fd76
ray-x <rayx.cn@gmail.com>
2022-06-29 10:03:53 +1000

    fix neovim 0.6.x

--- lua/tests/go_impl_spec.lua
+++ lua/tests/go_impl_spec.lua
@@ -6,11 +6,6 @@ local cur_dir = vim.fn.expand("%:p:h")
 -- status = require("plenary.reload").reload_module("nvim-treesitter")
 -- local ulog = require('go.utils').log
 describe("should run impl", function()
-  vim.cmd([[packadd go.nvim]])
-  vim.cmd([[packadd nvim-treesitter]])
-  status = require("plenary.reload").reload_module("go.nvim")
-  status = require("plenary.reload").reload_module("nvim-treesitter/nvim-treesitter")
-  require("go").setup({ verbose = true })
   it("should run impl", function()
     --
     if vim.fn.has("nvim-0.7") == 0 then
@@ -18,6 +13,12 @@ describe("should run impl", function()
       return eq(1, 1)
     end
 
+    vim.cmd([[packadd go.nvim]])
+    vim.cmd([[packadd nvim-treesitter]])
+    status = require("plenary.reload").reload_module("go.nvim")
+    status = require("plenary.reload").reload_module("nvim-treesitter/nvim-treesitter")
+
+    require("go").setup({ verbose = true })
     local name = vim.fn.tempname() .. ".go"
     local path = cur_dir .. "/lua/tests/fixtures/impl/impl_input.go" -- %:p:h ? %:p
     local lines = vim.fn.readfile(path)
