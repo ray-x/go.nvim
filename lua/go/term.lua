d02b08c81b8dc09a61174a43748157fefa28c036
rayx <rayx.cn@gmail.com>
2022-07-02 12:32:37 +1000

    Update term.lua

--- lua/go/term.lua
+++ lua/go/term.lua
@@ -48,18 +48,12 @@ local term = function(opts)
   if opts.autoclose == nil then
     opts.autoclose = true
   end
-
-  utils.log("logging cmds that will get pushed to floating_term - before table check")
-  utils.log(opts.cmd)
-
+  -- run in neovim shell
   if type(opts.cmd) == "table" then
       opts.cmd = table.concat(opts.cmd, " ")
-      utils.log("transformed cmd to string")
-      utils.log(opts.cmd)
   end
-
-
-
+  
+  utils.log(opts)
   local buf, win, closer = guihua_term.floating_term(opts)
   api.nvim_command("setlocal nobuflisted")
   api.nvim_buf_set_var(cur_buf, "go_float_terminal_win", { buf, win })
