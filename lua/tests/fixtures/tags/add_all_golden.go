4599360f5d9210631303873e53ef3b87bf663c9e
Shaw Vrana <shaw@vranix.com>
2022-11-26 22:33:53 -0800

    Add ability to set gomodifytag options in config (#246)

--- lua/tests/fixtures/tags/add_all_golden.go
+++ lua/tests/fixtures/tags/add_all_golden.go
@@ -1,17 +1,17 @@
 package main
 
 type Server struct {
-	Name          string   `json:"name"`
-	ID            int      `json:"id"`
-	MyHomeAddress string   `json:"my_home_address"`
-	SubDomains    []string `json:"sub_domains"`
-	Empty         string   `json:"empty"`
-	Example       int64    `json:"example"`
-	Example2      string   `json:"example_2"`
+	Name          string   `json:"name,omitempty"`
+	ID            int      `json:"id,omitempty"`
+	MyHomeAddress string   `json:"my_home_address,omitempty"`
+	SubDomains    []string `json:"sub_domains,omitempty"`
+	Empty         string   `json:"empty,omitempty"`
+	Example       int64    `json:"example,omitempty"`
+	Example2      string   `json:"example_2,omitempty"`
 	Bar           struct {
-		Four string `json:"four"`
-		Five string `json:"five"`
-	} `json:"bar"`
-	Lala interface{} `json:"lala"`
+		Four string `json:"four,omitempty"`
+		Five string `json:"five,omitempty"`
+	} `json:"bar,omitempty"`
+	Lala interface{} `json:"lala,omitempty"`
 } // eos
 // eof
