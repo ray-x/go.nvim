5adafc5e450e6c378795be0012202af98eeaa0a4
ray-x <rayx.cn@gmail.com>
2023-03-19 15:34:01 +1100

    ginkgo run test

--- lua/go/ginkgo.lua
+++ lua/go/ginkgo.lua
@@ -99,29 +99,43 @@ end
 M.test_func = function(...)
   local args = { ... }
   log(args)
-  local optarg = getopt.get_opts(args, short_opts, long_opts)
+  local optarg = {}
   local fpath = vfn.expand('%:p:h')
 
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   row, col = row, col + 1
 
-  local fnum = row - 3
+  local fnum = row - 60
   if fnum < 0 then
     fnum = 0
   end
   local lines = vim.api.nvim_buf_get_lines(0, fnum, row + 1, true)
 
   local describe = find_describe(lines)
-  log('testing: ', describe)
   if describe == nil then
-    log('failed to find test function, test file instead')
-    return M.test_file(args)
+    log('failed to find test function, test file instead', args)
+    return M.test_file(unpack(args))
   end
   local test_runner = 'ginkgo'
   require('go.install').install(test_runner)
 
+  -- local type_patterns = {
+  --   'function',
+  --   'method',
+  --   'type_spec',
+  --   'call_expression',
+  -- }
+  --
+  -- local f = require('nvim-treesitter').statusline({
+  --   indicator_size = 400,
+  --   type_patterns = type_patterns,
+  -- })
+  -- local ctx = vim.split(f, '->')
+  -- -- find describe
+  -- log(ctx)
+  --
   -- local cmd = { test_runner, [[ --focus=']] .. describe .. [[']], get_build_tags(args), fpath }
-  local cmd = { test_runner, [[ --focus-file=']] .. describe .. [[']], get_build_tags(args), fpath }
+  local cmd = { test_runner, [[ --focus=']] .. describe .. [[']], get_build_tags(args), fpath }
   log(cmd)
   if _GO_NVIM_CFG.run_in_floaterm then
     local term = require('go.term').run
@@ -131,7 +145,7 @@ M.test_func = function(...)
   cmd = [[setl makeprg=]] .. test_runner
   vim.cmd(cmd)
 
-  args = { [[ --focus-file=']] .. describe .. [[']], get_build_tags(args), fpath }
+  args = { [[ --focus=']] .. describe .. [[']], get_build_tags(args), fpath }
   require('go.asyncmake').make(unpack(args))
   utils.log('test cmd', cmd)
   return true
@@ -186,10 +200,9 @@ M.run = function(opts)
   if opts[1] then
     cmd = { test_runner, opts[1] }
     if opts[1] == 'bootstrap' then
-
       if not opts[2] then
         cwd = vim.fn.expand('%:p:h')
-        args = {cwd = cwd}
+        args = { cwd = cwd }
       end
     end
     if opts[2] then
