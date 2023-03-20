933ff9f0d84975122ec62ef98f78511db5c313c1
ray-x <rayx.cn@gmail.com>
2023-03-20 23:13:56 +1100

    improve treesitter inject

--- after/queries/go/injections.scm
+++ after/queries/go/injections.scm
@@ -1,10 +1,34 @@
-;; extends
+;; extends match Query and json
 (
  (const_spec
   name: (identifier) @_id
   value: (expression_list (raw_string_literal) @sql))
 
  (#contains? @_id "Query")
+)
+
+(
+ (const_spec
+  name: (identifier) @_id
+  value: (expression_list (raw_string_literal) @json))
+
+ (#contains? @_id "Json")
+)
+
+
+(short_var_declaration
+    left: (expression_list
+            (identifier) @_id (#match? @_id "Query"))
+    right: (expression_list
+             (raw_string_literal) @sql (#offset! @sql 0 1 0 -1))
+)
+
+
+(short_var_declaration
+    left: (expression_list
+            (identifier) @_id (#match? @_id "Json"))
+    right: (expression_list
+             (raw_string_literal) @json (#offset! @json 0 1 0 -1))
 )
 
 ((composite_literal
