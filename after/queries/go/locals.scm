09ed15f9c488cfe74eed442bbc2fe5e11b2d1803
Amaan Qureshi <amaanq12@gmail.com>
2023-03-29 12:31:04 -0400

    feat: improve queries

--- after/queries/go/locals.scm
+++ after/queries/go/locals.scm
@@ -1,15 +1,15 @@
-;; extends
+; extends
+
+(var_spec) @scope
+
 (field_declaration
   name: (field_identifier) @definition.field)
 
 (method_spec
   name: (field_identifier) @method.name
-  parameters:(parameter_list) @method.parameter_list
-)@interface.method.declaration
+  parameters: (parameter_list) @method.parameter_list) @interface.method.declaration
 
 (type_declaration
   (type_spec
     name: (type_identifier) @name
     type: [(struct_type) (interface_type)] @type)) @start
-
-(var_spec) @scope
