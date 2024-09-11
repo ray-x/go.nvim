d865da50a8609a4cfc9d9c5504e8e8d2b8f4a663
ray-x <rayx.cn@gmail.com>
2024-09-11 10:27:39 +1000

    fix ts subtest

--- lua/go/ts/go.lua
+++ lua/go/ts/go.lua
@@ -209,13 +209,10 @@ M.get_tbl_testcase_node_name = function(bufnr)
   for _, match, _ in tbl_case_query:iter_matches(tree:root(), bufn, 0, -1) do
     local tc_name = nil
     for id, node in pairs(match) do
-      debug('id', id, 'node', node)
       local name = tbl_case_query.captures[id]
       -- IDK why test.name is captured before test.block
       if name == 'test.name' then
         tc_name = vim.treesitter.get_node_text(node, bufn)
-        debug('names', tc_names)
-        -- return tc_name
       end
 
       if name == 'test.block' then
@@ -255,8 +252,7 @@ M.get_sub_testcase_name = function(bufnr)
       goto continue
     end
     if name == 'tc.name' and is_inside_test then
-      local tc_name = vim.treesitter.get_node_text(node, bufn)
-      return tc_name[1]
+      return vim.treesitter.get_node_text(node, bufn)
     end
     ::continue::
   end
