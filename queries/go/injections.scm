a2307e50183f7f4f27e5685e867c09321fb95913
ray-x <rayx.cn@gmail.com>
2024-12-01 14:53:27 +1100

    remove query failed to work in latest ts parser

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -2,24 +2,24 @@
 
 ; inject sql in single line strings
 ; e.g. db.GetContext(ctx, "SELECT * FROM users WHERE name = 'John'")
-
-((call_expression
-  (selector_expression
-    field: (field_identifier) @_field)
-  (argument_list
-    (interpreted_string_literal) @sql))
-  (#any-of? @_field "Exec" "GetContext" "ExecContext" "SelectContext" "In"
-				            "RebindNamed" "Rebind" "Query" "QueryRow" "QueryRowxContext" "NamedExec" "MustExec" "Get" "Queryx")
-  (#offset! @sql 0 1 0 -1))
-
-; still buggy for nvim 0.10
-((call_expression
-  (selector_expression
-    field: (field_identifier) @_field (#any-of? @_field "Exec" "GetContext" "ExecContext" "SelectContext" "In" "RebindNamed" "Rebind" "Query" "QueryRow" "QueryRowxContext" "NamedExec" "MustExec" "Get" "Queryx"))
-  (argument_list
-    (interpreted_string_literal) @injection.content))
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "sql"))
+; following no longer works after https://github.com/tree-sitter/tree-sitter-go/commit/47e8b1fae7541f6e01cead97201be19321ec362a
+; ((call_expression
+;   (selector_expression
+;     field: (field_identifier) @_field)
+;   (argument_list
+;     (interpreted_string_literal) @sql))
+;   (#any-of? @_field "Exec" "GetContext" "ExecContext" "SelectContext" "In"
+; 				            "RebindNamed" "Rebind" "Query" "QueryRow" "QueryRowxContext" "NamedExec" "MustExec" "Get" "Queryx")
+;   (#offset! @sql 0 1 0 -1))
+;
+; ; still buggy for nvim 0.10
+; ((call_expression
+;   (selector_expression
+;     field: (field_identifier) @_field (#any-of? @_field "Exec" "GetContext" "ExecContext" "SelectContext" "In" "RebindNamed" "Rebind" "Query" "QueryRow" "QueryRowxContext" "NamedExec" "MustExec" "Get" "Queryx"))
+;   (argument_list
+;     (interpreted_string_literal) @injection.content))
+;   (#offset! @injection.content 0 1 0 -1)
+;   (#set! injection.language "sql"))
 
 ; neovim nightly 0.10
 ([
