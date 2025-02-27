6ad080424f4b96a584cf591721e8e13c102cce4e
DanWlker <60170554+DanWlker@users.noreply.github.com>
2025-02-27 20:03:23 +0800

    fix(injections): make json injections work in nvim 0.10.0 (#552)

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -86,21 +86,30 @@
 
 (const_spec
   name: (identifier)
-  value: (expression_list (raw_string_literal) @injection.content
-   (#lua-match? @injection.content "^`[\n|\t| ]*\{.*\}[\n|\t| ]*`$")
-   (#offset! @injection.content 0 1 0 -1)
-   (#set! injection.language "json")))
+  value: (expression_list
+	   (raw_string_literal
+	     (raw_string_literal_content) @injection.content
+             (#lua-match? @injection.content "^[\n|\t| ]*\{.*\}[\n|\t| ]*$")
+             (#set! injection.language "json")
+	    )
+   ))
 
 (short_var_declaration
     left: (expression_list (identifier))
-    right: (expression_list (raw_string_literal) @injection.content)
-  (#lua-match? @injection.content "^`[\n|\t| ]*\{.*\}[\n|\t| ]*`$")
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "json"))
+    right: (expression_list
+             (raw_string_literal
+               (raw_string_literal_content) @injection.content
+               (#lua-match? @injection.content "^[\n|\t| ]*\{.*\}[\n|\t| ]*$")
+               (#set! injection.language "json"))
+               )
+    )
 
 (var_spec
   name: (identifier)
-  value: (expression_list (raw_string_literal) @injection.content
-   (#lua-match? @injection.content "^`[\n|\t| ]*\{.*\}[\n|\t| ]*`$")
-   (#offset! @injection.content 0 1 0 -1)
-   (#set! injection.language "json")))
+  value: (expression_list
+           (raw_string_literal
+             (raw_string_literal_content) @injection.content
+             (#lua-match? @injection.content "^[\n|\t| ]*\{.*\}[\n|\t| ]*$")
+             (#set! injection.language "json")
+             )
+   ))
