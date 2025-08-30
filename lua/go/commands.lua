67a2c060a8b3bf73dce9345c92b27ee957b7aee3
code-winder <118537583+code-winder@users.noreply.github.com>
2025-08-30 10:33:28 +0000

    减少Go打头的命令

--- lua/go/commands.lua
+++ lua/go/commands.lua
@@ -138,40 +138,9 @@ return {
     augroup end
     ]])
 
-    create_cmd('GoMake', function(_)
-      require('go.asyncmake').make()
-    end)
-
-    create_cmd('GoFmt', function(opts)
-      require('go.format').gofmt(unpack(opts.fargs))
-    end, { nargs = '*' })
-
-    create_cmd('GoImport', function(opts)
-      vim.notify('GoImport is deprecated, use GoImports')
-      require('go.format').goimports(unpack(opts.fargs))
-    end, {})
-    create_cmd('GoImports', function(opts)
-      require('go.format').goimports(unpack(opts.fargs))
-    end, {
-      complete = function(a, l)
-        return package.loaded.go.doc_complete(a, l)
-      end,
-      nargs = '*',
-    })
-
-    create_cmd('GoGet', function(opts)
-      require('go.goget').run(opts.fargs)
-    end, { nargs = '*' })
-
     local gobin = _GO_NVIM_CFG.go
     local cmd = string.format(
       [[command! -nargs=* GoGenerate :setl makeprg=%s\ generate | lua require'go.asyncmake'.make(<f-args>)]],
-      gobin
-    )
-    vim.cmd(cmd)
-
-    cmd = string.format(
-      [[command! -nargs=* -complete=customlist,v:lua.package.loaded.go.package_complete GoBuild :setl makeprg=%s\ build | lua require'go.asyncmake'.make(<f-args>)]],
       gobin
     )
     vim.cmd(cmd)
@@ -181,27 +150,7 @@ return {
       gobin
     )
     vim.cmd(cmd)
-    local pcmdstr = ''
-    local preludes = _GO_NVIM_CFG.preludes
-    local gorun_preludes = preludes.GoRun or preludes.default
-    if gorun_preludes ~= nil then
-      local pcmd = gorun_preludes()
-      if #pcmd > 0 then
-        pcmdstr = table.concat(pcmd, '\\ ') .. '\\ '
-      end
-    end
 
-    cmd = string.format(
-      [[command! -nargs=* -complete=customlist,v:lua.package.loaded.go.package_complete GoRun   :setl makeprg=%s%s\ run | lua require'go.asyncmake'.make(<f-args>)]],
-      pcmdstr,
-      gobin
-    )
-    utils.log(cmd)
-    vim.cmd(cmd)
-
-    create_cmd('GoStop', function(opts)
-      require('go.asyncmake').stopjob(unpack(opts.fargs))
-    end, { nargs = '*' })
     -- if you want to output to quickfix
 
     -- example to running test in split buffer
@@ -327,32 +276,7 @@ return {
     create_cmd('GoAddAllTest', function(opts)
       require('go.gotests').all_test(unpack(opts.fargs))
     end, { nargs = '*' })
-    create_cmd('GoModVendor', function(opts)
-      require('go.mod').run('vendor', unpack(opts.fargs))
-    end, { nargs = '*' })
-    create_cmd('GoModDnld', function(opts)
-      require('go.mod').run('download', unpack(opts.fargs))
-    end, { nargs = '*' })
 
-    create_cmd('GoModGraph', function(opts)
-      require('go.mod').run('graph', unpack(opts.fargs))
-    end, { nargs = '*' })
-    create_cmd('GoModWhy', function(opts)
-      if #opts.fargs == 0 then
-        local m = require('go.mod').get_mod()
-        if m then
-          require('go.mod').run('why', m)
-          return
-        end
-      end
-      require('go.mod').run('why', unpack(opts.fargs))
-    end, { nargs = '*' })
-    create_cmd('GoModInit', function(opts)
-      require('go.mod').run('init', unpack(opts.fargs))
-    end, { nargs = '*' })
-    create_cmd('GoEnv', function(opts)
-      require('go.env').load_env(unpack(opts.fargs))
-    end, { nargs = '*' })
 
     create_cmd('GoCodeLenAct', function(_)
       require('go.codelens').run_action()
@@ -457,16 +381,6 @@ return {
       require('go.fixplurals').fixplurals()
     end)
 
-    create_cmd('GoAlt', function(opts)
-      require('go.alternate').switch(opts.bang, '')
-    end, { bang = true })
-    create_cmd('GoAltV', function(opts)
-      require('go.alternate').switch(opts.bang, 'vsplit')
-    end, { bang = true })
-    create_cmd('GoAltS', function(opts)
-      require('go.alternate').switch(opts.bang, 'split')
-    end, { bang = true })
-
     create_cmd('GoWork', function(opts)
       require('go.work').run(unpack(opts.fargs))
     end, {
@@ -475,9 +389,7 @@ return {
         return { 'run', 'use' }
       end,
     })
-    create_cmd('GoModTidy', function(_)
-      require('go.gopls').tidy()
-    end)
+
     create_cmd('GoListImports', function(_)
       local lines = require('go.gopls').list_imports().PackageImports or {}
 
@@ -505,26 +417,9 @@ return {
       end,
     })
 
-    create_cmd('GoEnv', function(opts)
-      require('go.env').load_env(unpack(opts.fargs))
-    end, { nargs = '*' })
-
     create_cmd('GoGenReturn', function()
       require('go.lsp').hover_returns()
     end)
-
-    create_cmd('GoJson2Struct', function(opts)
-      require('go.json2struct').run(opts)
-    end, {
-      nargs = '*',
-      bang = true,
-      register = true,
-      -- complete = function(ArgLead, CmdLine, CursorPos)
-      complete = function(_, _, _)
-        return { 'myStruct' }
-      end,
-      range = true,
-    })
 
     create_cmd('Gomvp', function(opts)
       require('go.gomvp').run(opts.fargs)
@@ -550,25 +445,7 @@ return {
         return require('go.template.gonew').complete
       end,
     })
-    create_cmd('Ginkgo', function(opts)
-      require('go.ginkgo').run(opts.fargs)
-    end, {
-      nargs = '*',
-      complete = function(_, _, _)
-        -- return completion candidates as a list-like table
-        return { 'generate', 'bootstrap', 'build', 'labels', 'run', 'watch' }
-      end,
-    })
-    create_cmd('GinkgoFunc', function(opts)
-      require('go.ginkgo').test_func(opts.fargs)
-    end, {
-      nargs = '*',
-    })
-    create_cmd('GinkgoFile', function(opts)
-      require('go.ginkgo').test_file(opts.fargs)
-    end, {
-      nargs = '*',
-    })
+
     create_cmd('GoToggleInlay', function(opts)
       local enabled = vim.lsp.inlay_hint.is_enabled()
       vim.lsp.inlay_hint.enable(not enabled)
